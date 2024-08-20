9938971cb8c906ca4a1cb865c4725f1859be0aa6
vishnu <kotharuvishnu@gmail.com>
2024-08-20 11:32:43 +0530

    updates

---
+++ day-3/main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "s3" {
+    bucket = "test123bucket123-trst123"
+
+}
---
+++ day-3/variable.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    description = "passing the value to main "
+    type = string
+    default = ""
+  
+}
+
+variable "instance_type" {
+    description = "passing the value to main "
+    type = string
+    default = ""
+  
+}
+
+variable "key_name" {
+    description = "passing the value to main"
+    type = string
+    default = ""
+  
+}
